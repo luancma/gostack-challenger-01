d1b47165841b57cbbefca4a874fe30b45093c81f
computador <computador@localhost.localdomain>
2019-10-12 12:45:17 -0300

    created the routes

--- index.js
+++ index.js
@@ -4,8 +4,57 @@ const server = express();
 
 server.use(express.json());
 
-server.get("/", (req, res) => {
-  return res.json({ message: "Hello world" });
+//Initial value of projects
+const projects = [];
+
+// Routes
+server.get("/projects", (req, res) => {
+  return res.json(projects);
 });
 
+server.post("/projects", (req, res) => {
+  const { title, id } = req.body;
+
+  const project = {
+    id,
+    title,
+    tasks: []
+  };
+
+  projects.push(project);
+
+  return res.json(project);
+});
+
+server.delete("/projects/:id", (req, res) => {
+  const { id } = req.params;
+
+  const projectId = projects.find(i => i.id === id);
+
+  projects.splice(projectId, 1);
+
+  return res.send();
+});
+
+server.put("/projects/:id", (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(i => i.id == id);
+
+  project.title = title;
+
+  return res.json(project);
+});
+
+server.post("/projects/:id/tasks", (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(i => i.id == id);
+
+  project.tasks.push(title);
+
+  return res.json(project);
+});
 server.listen(3333);
